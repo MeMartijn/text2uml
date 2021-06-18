490e7aa6c8d84e63df56339e14bad6bcb0f596f4
MeMartijn <martijn.schouten@icloud.com>
2021-06-18 14:06:36 +0200

    chore: added seperate experiments for all embeddings

---
+++ metadata-classification/embeddings_xlm.py
@@ -0,0 +1,25 @@
+'''
+Python script for generating embeddings on ALICE computing facility
+'''
+
+import socket
+from time import time
+
+# Adapted from https://github.com/MeMartijn/FakeNewsDetection
+from data_loader import DataLoader  
+
+if __name__ == '__main__':
+    # Get starting time of script
+    start_time = time()
+
+    print('Python test started on {}'.format(socket.gethostname()))
+
+    # Initialize data loading module
+    data = DataLoader()
+
+    # Get word embeddings
+    print('Start loading XLM...')
+    embeddings = data.get_xlm()
+    print('Finished generating XLM')
+
+    print('Python test finished (running time: {0:.1f}s)'.format(time() - start_time))
