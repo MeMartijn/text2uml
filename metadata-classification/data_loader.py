fc73c2f3839cf11f5809e9effb5914cf253fc003
MeMartijn <martijn.schouten@icloud.com>
2021-07-28 10:16:28 +0200

    refactor: created abstraction of encoder and inheritance structure for flairencoder

--- metadata-classification/data_loader.py
+++ metadata-classification/data_loader.py
@@ -8,14 +8,14 @@ import torch
 
 # Flair embedding imports
 from flair.data import Sentence
-from flair.embeddings import TransformerWordEmbeddings, WordEmbeddings, FlairEmbeddings, StackedEmbeddings, ELMoEmbeddings
+from flair.embeddings import TransformerWordEmbeddings
 from transformers import TransfoXLModel, TransfoXLTokenizer, XLNetModel, XLNetTokenizer, XLMModel, XLMTokenizer
 import os
 from nltk import tokenize
 
-class FlairEncoder:
-    '''An interface for interacting with Zalando's Flair library'''
-    def __init__(self, embedding, data_dir, data):
+class GeneralEncoder:
+    '''An interface for interacting with the fasttext model'''
+    def __init__(self, data_dir, data, embedding = None):
         # Prepare tqdm loops
         tqdm.pandas()
 
@@ -23,45 +23,24 @@ class FlairEncoder:
         self.embedding = embedding
         self.data_dir = data_dir
         self.dfs = data
+    
+    def get_embedder(self):
+        '''Empty script for setting custom embedder'''
+        raise ValueError('This function should be implemented in the children encoders')
+    
+    def create_embedding(self, statement, embedder):
+        '''Create a single embedding from a piece of text'''
+        raise ValueError('This function should be implemented in the children encoders')
+    
+    def get_embedding_dir(self, embedding):
+        '''Turn the name of the embedding technique into a specific folder'''
+        if '/' in embedding:
+            return embedding.split('/')[1]
+        else:
+            return embedding
 
     def get_embedded_dataset(self, save = True):
         '''Return the embedding representation of the dataset'''
-        def get_embedding_dir(embedding):
-            '''Turn the name of the embedding technique into a specific folder'''
-            if embedding[-1:] == ')':
-                # The embedding technique is part of a function
-                return re.search(r'"(.+)"', embedding).group(1)
-            elif '/' in embedding:
-                return embedding.split('/')[1]
-
-            return embedding
-
-        def create_embedding(statement, embedding):
-            '''Create a single embedding from a piece of text'''
-            # Split all sentences
-            sentences = tokenize.sent_tokenize(statement)
-
-            # Create an array for storing the embeddings
-            vector = []
-
-            # Loop over all sentences and apply embedding
-            for sentence in sentences:
-                if isinstance(embedding, dict):
-                    # Get embedding from Huggingface directly
-                    input_ids = torch.tensor(embedding['tokenizer'].encode(sentence)).unsqueeze(0)
-                    outputs = embedding['model'](input_ids)
-                    last_hidden_states = outputs[0]
-                    vector.append(list(last_hidden_states[0].detach().numpy()))
-                else:
-                    # Continue as "regular" Flair-based embedding
-                    # Create a Sentence object for each sentence in the statement
-                    sentence = Sentence(sentence, use_tokenizer = True)
-
-                    # Embed words in sentence
-                    embedding.embed(sentence)
-                    vector.append([token.embedding.cpu().numpy() for token in sentence])
-
-            return vector
         
         def encode_datasets(embedding_dir, data_dir, dfs, embedding):
             '''Return all datasets with embeddings instead of texts'''
@@ -74,38 +53,11 @@ class FlairEncoder:
             else:
                 print('Creating representations and saving them as files...')
 
-                # Activate embedding
-                if embedding_dir == 'flair':
-                    # Flair's recommended usage is different from other embedding techniques
-                    embedding = StackedEmbeddings([
-                        WordEmbeddings('glove'),
-                        FlairEmbeddings('news-forward'),
-                        FlairEmbeddings('news-backward'),
-                    ])
-                elif embedding[-1:] == ')':
-                    # This embedding has parameters
-                    embedding = eval(embedding)
-                elif embedding == 'transfo-xl-wt103':
-                    embedding = {
-                        'model': TransfoXLModel.from_pretrained('transfo-xl-wt103'),
-                        'tokenizer': TransfoXLTokenizer.from_pretrained('transfo-xl-wt103'),
-                    }
-                elif embedding == 'xlm-mlm-en-2048':
-                    embedding = {
-                        'model': XLMModel.from_pretrained('transfo-xl-wt103'),
-                        'tokenizer': XLMTokenizer.from_pretrained('transfo-xl-wt103'),
-                    }
-                elif embedding == 'xlnet-base-cased':
-                    embedding = {
-                        'model': XLNetModel.from_pretrained('transfo-xl-wt103'),
-                        'tokenizer': XLNetTokenizer.from_pretrained('transfo-xl-wt103'),
-                    }
-                else:
-                    embedding = TransformerWordEmbeddings(embedding)
+                embedder = self.get_embedder()
 
                 # Apply transformation
                 dfs['embedding'] = dfs['name'].progress_map(
-                    lambda text: create_embedding(text, embedding)
+                    lambda text: self.create_embedding(text, embedder)
                 )
 
                 print('Make sure this is okay:')
@@ -124,19 +76,181 @@ class FlairEncoder:
                 return dfs
         
         # Directory name for saving the datasets
-        embedding_dir = get_embedding_dir(self.embedding)
+        embedding_dir = self.get_embedding_dir(self.embedding)
 
         return encode_datasets(embedding_dir, self.data_dir, self.dfs, self.embedding)
+
+class FlairEncoder(GeneralEncoder):
+    '''An interface for interacting with Zalando's Flair library'''
+    def get_embedder(self):
+        # Activate embedding
+        if self.embedding == 'transfo-xl-wt103':
+            embedding = {
+                'model': TransfoXLModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': TransfoXLTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        elif self.embedding == 'xlm-mlm-en-2048':
+            embedding = {
+                'model': XLMModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': XLMTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        elif self.embedding == 'xlnet-base-cased':
+            embedding = {
+                'model': XLNetModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': XLNetTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        else:
+            embedding = TransformerWordEmbeddings(self.embedding)
+        
+        return embedding
+    
+    def create_embedding(self, statement, embedder):
+        '''Create a single embedding from a piece of text'''
+        # Split all sentences
+        sentences = tokenize.sent_tokenize(statement)
+
+        # Create an array for storing the embeddings
+        vector = []
+
+        # Loop over all sentences and apply embedding
+        for sentence in sentences:
+            if isinstance(embedder, dict):
+                # Get embedding from Huggingface directly
+                input_ids = torch.tensor(embedder['tokenizer'].encode(sentence)).unsqueeze(0)
+                outputs = embedder['model'](input_ids)
+                last_hidden_states = outputs[0]
+                vector.append(list(last_hidden_states[0].detach().numpy()))
+            else:
+                # Continue as "regular" Flair-based embedding
+                # Create a Sentence object for each sentence in the statement
+                sentence = Sentence(sentence, use_tokenizer = True)
+
+                # Embed words in sentence
+                embedder.embed(sentence)
+                vector.append([token.embedding.cpu().numpy() for token in sentence])
+
+        return vector
+
+class FastTextEncoder(GeneralEncoder):
+    '''An interface for interacting with fastText'''
+    # TODO: IMPLEMENT FASTTEXT ENCODER
+    def get_embedder(self):
+        # Activate embedding
+        if self.embedding == 'transfo-xl-wt103':
+            embedding = {
+                'model': TransfoXLModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': TransfoXLTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        elif self.embedding == 'xlm-mlm-en-2048':
+            embedding = {
+                'model': XLMModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': XLMTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        elif self.embedding == 'xlnet-base-cased':
+            embedding = {
+                'model': XLNetModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': XLNetTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        else:
+            embedding = TransformerWordEmbeddings(self.embedding)
+        
+        return embedding
+    
+    def create_embedding(self, statement, embedder):
+        '''Create a single embedding from a piece of text'''
+        # Split all sentences
+        sentences = tokenize.sent_tokenize(statement)
+
+        # Create an array for storing the embeddings
+        vector = []
+
+        # Loop over all sentences and apply embedding
+        for sentence in sentences:
+            if isinstance(self.transformer, dict):
+                # Get embedding from Huggingface directly
+                input_ids = torch.tensor(self.transformer['tokenizer'].encode(sentence)).unsqueeze(0)
+                outputs = self.transformer['model'](input_ids)
+                last_hidden_states = outputs[0]
+                vector.append(list(last_hidden_states[0].detach().numpy()))
+            else:
+                # Continue as "regular" Flair-based embedding
+                # Create a Sentence object for each sentence in the statement
+                sentence = Sentence(sentence, use_tokenizer = True)
+
+                # Embed words in sentence
+                self.transformer.embed(sentence)
+                vector.append([token.embedding.cpu().numpy() for token in sentence])
+
+        return vector
+
+
+class Word2VecEncoder(GeneralEncoder):
+    '''An interface for interacting with fastText'''
+    # TODO: IMPLEMENT WORD2VEC ENCODER
+    def get_embedder(self):
+        # Activate embedding
+        if self.embedding == 'transfo-xl-wt103':
+            embedding = {
+                'model': TransfoXLModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': TransfoXLTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        elif self.embedding == 'xlm-mlm-en-2048':
+            embedding = {
+                'model': XLMModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': XLMTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        elif self.embedding == 'xlnet-base-cased':
+            embedding = {
+                'model': XLNetModel.from_pretrained('transfo-xl-wt103'),
+                'tokenizer': XLNetTokenizer.from_pretrained('transfo-xl-wt103'),
+            }
+        else:
+            embedding = TransformerWordEmbeddings(self.embedding)
+        
+        return embedding
+    
+    def create_embedding(self, statement, embedder):
+        '''Create a single embedding from a piece of text'''
+        # Split all sentences
+        sentences = tokenize.sent_tokenize(statement)
+
+        # Create an array for storing the embeddings
+        vector = []
+
+        # Loop over all sentences and apply embedding
+        for sentence in sentences:
+            if isinstance(self.transformer, dict):
+                # Get embedding from Huggingface directly
+                input_ids = torch.tensor(self.transformer['tokenizer'].encode(sentence)).unsqueeze(0)
+                outputs = self.transformer['model'](input_ids)
+                last_hidden_states = outputs[0]
+                vector.append(list(last_hidden_states[0].detach().numpy()))
+            else:
+                # Continue as "regular" Flair-based embedding
+                # Create a Sentence object for each sentence in the statement
+                sentence = Sentence(sentence, use_tokenizer = True)
+
+                # Embed words in sentence
+                self.transformer.embed(sentence)
+                vector.append([token.embedding.cpu().numpy() for token in sentence])
+
+        return vector
+
 
 
 class DataLoader:
     '''A class which holds functionality to load and interact with the data from the research article'''
-    def __init__(self):
+    def __init__(self, dataset = 'lindholmen'):
         # Prepare tqdm loops
         tqdm.pandas()
 
         # Set target data file
-        self.data_link = '../../data/uml_extracted_metadata_annotated.json'
+        if dataset == 'lindholmen':
+            self.data_link = '../../data/uml_extracted_metadata_annotated.json'
+        elif dataset == 'genmymodel':
+            self.data_link = '../../data/genmymodel_uml_extracted_metadata_annotated.json'
+        else:
+            raise ValueError('This dataset is not supported. Please only use \'lindholmen\' or \'genmymodel\' as keywords.')
 
         # Set target directory for saving embeddings
         self.data_dir = '../embeddings'
@@ -181,27 +295,33 @@ class DataLoader:
 
         # Create big dataframe with all values together
         flatten = lambda t: [item for sublist in t for item in sublist]
-        return pd.DataFrame(list(map(lambda x: [x, 'class'], flatten(all_classes))) + list(map(lambda x: [x, 'attribute'], flatten(all_attrs))), columns=['name', 'type'])
+        return pd.DataFrame(list(map(lambda x: [x, 'class'], flatten(all_classes))) + list(map(lambda x: [x, 'attribute'], flatten(all_attrs))), columns=['name', 'type'])[:10]
 
     def set_embeddings(self):
         '''Set all interfaces for embedding techniques using custom functions or Flair encoders'''
         
         def get_flair_embedding(embedding):
-            encoder = FlairEncoder(embedding, self.data_dir, self.df)
+            encoder = FlairEncoder(embedding = embedding, data_dir = self.data_dir, data = self.df)
+            return encoder.get_embedded_dataset
+        
+        def get_fasttext_embedding():
+            encoder = FastTextEncoder(data_dir = self.data_dir, data = self.df) 
+            return encoder.get_embedded_dataset
+
+        def get_word2vec_embedding():
+            encoder = Word2VecEncoder(data_dir = self.data_dir, data = self.df) 
             return encoder.get_embedded_dataset
 
         # Attach all function references
         self.get_bert = get_flair_embedding('bert-base-uncased')
-        self.get_elmo = get_flair_embedding('ELMoEmbeddings()')
         self.get_transformerxl = get_flair_embedding('transfo-xl-wt103')
         self.get_gpt = get_flair_embedding('openai-gpt')
-        self.get_flair = get_flair_embedding('FlairEmbeddings')
-        self.get_fasttext = get_flair_embedding('WordEmbeddings("en-crawl")')
         self.get_gpt2 = get_flair_embedding('gpt2')
         self.get_xlm = get_flair_embedding('xlm-mlm-en-2048')
         self.get_xlnet = get_flair_embedding('xlnet-base-cased')
         self.get_gptneo = get_flair_embedding('EleutherAI/gpt-neo-1.3B')
-        self.get_roberta = get_flair_embedding('roberta-base')
+        self.get_fasttext = get_fasttext_embedding()
+        self.get_word2vec = get_word2vec_embedding()
     
     @staticmethod
     def apply_pooling(technique, df):
