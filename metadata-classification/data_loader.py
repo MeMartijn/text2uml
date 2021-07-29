b9a80d7ab540d11218d7bd81405a2f8c83904a55
MeMartijn <martijn.schouten@icloud.com>
2021-07-29 11:23:46 +0200

    test: temporarily disable saving for debugging purposes

--- metadata-classification/data_loader.py
+++ metadata-classification/data_loader.py
@@ -35,7 +35,7 @@ class GeneralEncoder:
         else:
             return embedding
 
-    def get_embedded_dataset(self, save = True):
+    def get_embedded_dataset(self, save = False):
         '''Return the embedding representation of the dataset'''
         
         def encode_datasets(embedding_dir, data_dir, df, embedding):
