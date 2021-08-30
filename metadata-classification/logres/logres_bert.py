9ee7687fdedbb4cde55c9ffb2cd7d48930e4e537
MeMartijn <martijn.schouten@icloud.com>
2021-08-30 10:53:18 +0200

    refactor: removed unneeded variables

--- metadata-classification/logres/logres_bert.py
+++ metadata-classification/logres/logres_bert.py
@@ -4,10 +4,6 @@ Python script for running metadata classification on ALICE computing facility
 
 import socket
 from time import time
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from hypopt import GridSearch
-from sklearn.metrics import classification_report
 import pandas as pd
 import sys
 
