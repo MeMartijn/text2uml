aa4e0fe3b8ec07c14586a2e0db7dc416fe39d873
MeMartijn <martijn.schouten@icloud.com>
2022-01-05 20:06:24 +0100

    refactor: moved from amalgram to booknlp

--- bucketization/bucketize.py
+++ bucketization/bucketize.py
@@ -1,59 +1,58 @@
-import spacy
 import pandas as pd
 import os
-from nltk import tokenize
+from booknlp.booknlp import BookNLP
 
-nlp = spacy.load("en_core_web_sm")
-
-def get_supersenses(sent):
-    doc = nlp(sent)
-    target_file = '../../pysupersensetagger/input'
-
-    if os.path.exists(target_file):
-        os.remove(target_file)
-        
-    with open(target_file, 'a') as inp_file:
-        for token in doc:
-            inp_file.write(f'{token}\t{token.tag_}\n')
-    
-    
-    os.system(f'cd {"/".join(target_file.split("/")[:-1])} && sh sst.sh {target_file.split("/")[-1]}')
-    
-    return pd.read_csv('../../pysupersensetagger/input.pred.tags', sep='\t', names=['token', 'lemma', 'POS-tag', 'MWE+supersense tag', 'MWE parent offset', 'MWE attachment strength', 'supersense label', 'sentence ID'])
-
-def get_metadata(supersense_df):
-    prepositions = supersense_df[supersense_df['POS-tag'] == 'IN']
-
-    metadata = {
-        'objects': []
+def get_supersenses(filename):
+    model_params={
+        "pipeline": "entity,supersense,event", 
+        "model": "small"
     }
 
-    # Check if there are groups of nouns
-    if len(prepositions) > 0:
-        # Define target groups
-        targets = ['NN', 'NNS']
-            
-        for pps in prepositions.iterrows():
-            # If ending or beginning position, it can't be a group of nouns
-            if not (pps[0] == 0 or pps[0] >= len(supersense_df) - 1):
-                # Define surroundings
-                preword = supersense_df.loc[pps[0] - 1]
-                postword = supersense_df.loc[pps[0] + 1]
-                
-                # Check if surroundings are in target groups
-                if preword['POS-tag'] in targets and postword['POS-tag'] in targets:
-                    # Add grouping to metadata
-                    metadata['objects'].append(preword.token + ' ' + pps[1].token + ' ' + postword.token)
-                    
-                    # Drop extracted groups from selection
-                    mask = supersense_df.index.isin(list(range(pps[0] -1, pps[0] + 2)))
-                    supersense_df = supersense_df[~mask]
-            
-        # Now get all other nouns and add to metadata
-        nouns = supersense_df[supersense_df['POS-tag'].isin(targets)]
-        metadata['objects'] = metadata['objects'] + nouns['token'].to_list()
+    booknlp = BookNLP('en', model_params)
+
+    # File identifier
+    file_id = '.'.join(filename.split('.')[:-1])
+
+    # Input file to process
+    input_file=f'./../../PurePlainDataset/output/{filename}'
+
+    # Output directory to store resulting files in
+    output_directory=f'output/{file_id}/'
+
+    # Publish results
+    booknlp.process(input_file, output_directory, file_id)
+
+    # Gather results
+    df = pd.read_csv(f'{output_directory}{file_id}.tokens', sep='\t')
+
+    # Gather supersenses
+    supersenses = pd.read_csv(f'{output_directory}{file_id}.supersense', sep='\t')
+
+    # Begin merging both dataframes
+    df['supersense_category'] = ''
+
+    # Loop over supersense df
+    for index, row in supersenses.iterrows():
+        # Add supersense to all rows in range of starting and ending tokens
+        for token in range(row.start_token, row.end_token + 1):
+            df.loc[df.token_ID_within_document == token, 'supersense_category'] = row.supersense_category
     
-    return metadata
+    # Gather entities
+    supersenses = pd.read_csv(f'{output_directory}{file_id}.entities', sep='\t')
+
+    # Begin merging entities
+    df['prop'] = ''
+    df['cat'] = ''
+    df['COREF'] = np.nan
+
+    # Loop over entity df
+    for index, row in supersenses.iterrows():
+        # Add entities to all rows in range of starting and ending tokens
+        for token in range(row.start_token, row.end_token + 1):
+            df.loc[df.token_ID_within_document == token, ['prop', 'cat', 'COREF']] = [row.prop, row['cat'], row.COREF]
+    
+    sent_groups = df.groupby('sentence_ID')
+    return [sent_groups.get_group(x) for x in sent_groups.groups]
 
 
 def is_important_for_class(supersense_df):
@@ -63,10 +62,8 @@ def is_important_for_class(supersense_df):
     else:
         return False
 
-def apply_bucketing(summary):
-    sentences = tokenize.sent_tokenize(summary)
-    
-    sentences_supersenses = [get_supersenses(sentence) for sentence in sentences]
+def apply_bucketing(file):
+    sentences_supersenses = get_supersenses(file)
     
     bucketed_data = {
         'classes': list(filter(lambda x: is_important_for_class(x), sentences_supersenses))
